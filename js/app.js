96cd688b4c5c5cb24633e576595f29d9fbd7334a
batch-05 <nasir.joypurhat@gmail.com>
2022-02-12 14:17:12 +0600

    submit button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('link up');
+
+// submit button 
+const submitButton = document.getElementById("detail-submit-btn");
+
+submitButton.addEventListener("click", function () {
+  const buyerDetails = document.getElementById("buyer-details-input");
+  console.log("hello input", buyerDetails);
+
+  document.getElementById("buyer-info").innerText = buyerDetails.value;
+
+  buyerDetails.value='';
+});
+
+
